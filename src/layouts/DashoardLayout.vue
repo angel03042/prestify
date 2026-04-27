5e10cf9fec53990162f2a96f5217c17ea4bc4458
Angel Benitez <angelbenitezramirez0@gmail.com>
2026-04-27 12:56:35 -0600

    refactor: estructura del dashboardLayout

---
+++ src/layouts/DashoardLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-neutral-900 min-h-screen grid grid-cols-[auto_1fr]">
+
+    <!-- Sidebar fijo -->
+    <Sidebar />
+
+    <div class="flex flex-col h-screen">
+
+      <!-- Navbar fijo -->
+      <Navbar />
+
+      <!-- Contenido dinámico -->
+      <main class="flex-1 overflow-y-auto p-6">
+        <slot />
+      </main>
+
+    </div>
+  </div>
+</template>
