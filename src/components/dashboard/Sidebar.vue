99e00ee220f72550b1ee052454621daa4d3fdeef
Angel Benitez <angelbenitezramirez0@gmail.com>
2026-04-29 23:25:23 -0600

    fix: correcion en el sidebar

--- src/components/dashboard/Sidebar.vue
+++ src/components/dashboard/Sidebar.vue
@@ -25,34 +25,23 @@ import router from '../../router/router';
 
           <router-link to="/dashboard/clientes" class="flex items-center p-3 rounded-xl text-zinc-400 hover:bg-white hover:text-black transition-all duration-200 overflow-hidden" active-class="bg-white/15 text-black">
               <div class="min-w-[32px] flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-package-search"><path d="M21 10V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l2-1.14"/><path d="m7.5 4.27 9 5.15"/><polyline points="3.29 7 12 12 20.71 7"/><line x1="12" x2="12" y1="22" y2="12"/><circle cx="18.5" cy="15.5" r="2.5"/><path d="M20.27 17.27 22 19"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
               </div>
               <span class="ml-4 font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">Clientes</span>
           </router-link>
 
           <router-link to="/dashboard/prestamos" class="flex items-center p-3 rounded-xl text-zinc-400 hover:bg-white hover:text-black transition-all duration-200 overflow-hidden" active-class="bg-white/15 text-black">
               <div class="min-w-[32px] flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17"/><path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9"/><circle cx="18" cy="5" r="3"/></svg>
               </div>
               <span class="ml-4 font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">Prestamos</span>
           </router-link>
 
-          <router-link to="/dashboard/tienda" class="flex items-center p-3 rounded-xl text-zinc-400 hover:bg-white hover:text-black transition-all duration-200 overflow-hidden" active-class="bg-white/15 text-black">
+          <router-link to="/dashboard/cartera" class="flex items-center p-3 rounded-xl text-zinc-400 hover:bg-white hover:text-black transition-all duration-200 overflow-hidden" active-class="bg-white/15 text-black">
               <div class="min-w-[32px] flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-store">
-                <path d="M15 21v-5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v5"/>
-                <path d="M17.774 10.31a1.12 1.12 0 0 0-1.549 0 2.5 2.5 0 0 1-3.451 0 1.12 1.12 0 0 0-1.548 0 2.5 2.5 0 0 1-3.452 0 1.12 1.12 0 0 0-1.549 0 2.5 2.5 0 0 1-3.77-3.248l2.889-4.184A2 2 0 0 1 7 2h10a2 2 0 0 1 1.653.873l2.895 4.192a2.5 2.5 0 0 1-3.774 3.244"/>
-                <path d="M4 10.95V19a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8.05"/>
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H5a2 2 0 0 0 0 4h15a1 1 0 0 0 1-1v-4"/><rect width="3" height="3" x="15" y="10" rx="1"/></svg>
               </div>
-              <span class="ml-4 font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">Tienda</span>
-          </router-link>
-
-          <router-link to="/dashboard/catalogo" class="flex items-center p-3 rounded-xl text-zinc-400 hover:bg-white hover:text-black transition-all duration-200 overflow-hidden" active-class="bg-white/15 text-black">
-              <div class="min-w-[32px] flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open-icon lucide-book-open"><path d="M12 7v14"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/></svg>
-              </div>
-              <span class="ml-4 font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">Catalogo</span>
+              <span class="ml-4 font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">Cartera</span>
           </router-link>
 
           <rouuter-link to="/dashboard/reportes" class="flex items-center p-3 rounded-xl text-zinc-400 hover:bg-white hover:text-black transition-all duration-200 overflow-hidden" active-class="bg-white/15 text-black">
@@ -68,20 +57,6 @@ import router from '../../router/router';
             Reportes
             </span>
           </rouuter-link>
-
-          <router-link to="/dashboard/historial" class="flex items-center p-3 rounded-xl text-zinc-400 hover:bg-white hover:text-black transition-all duration-200 overflow-hidden" active-class="bg-white/15 text-black">
-            <div class="min-w-[32px] flex justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-history">
-                <path d="M3 3v5h5"/>
-                <path d="M3.05 13a9 9 0 1 0 .5-4"/>
-                <path d="M12 7v5l3 3"/>
-              </svg>
-            </div>
-            <span class="ml-4 font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
-            Historial
-            </span>
-          </router-link>
-
       </div>
     </div>
 
