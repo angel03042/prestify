baa79eb14b78e00438dea66cfdae9c5843ebe627
Angel Benitez <angelbenitezramirez0@gmail.com>
2026-05-01 21:57:55 -0600

    refactor: logica para registrar usuarios

--- src/services/auth/register.js
+++ src/services/auth/register.js
@@ -1,6 +1,6 @@
 import { supa } from "@/lib/supabase.js";
 
-export const registerService = async (nombre, apellido, emial, password) => {
+export const registerService = async (nombre, apellido, email, password, terminos) => {
   try {
     const { data: authData, error: authError } = await supa.auth.signUp({
       email,
@@ -13,7 +13,7 @@ export const registerService = async (nombre, apellido, emial, password) => {
 
     const { data: userData, error: dbError } = await supa
       .from("usuarios")
-      .insert([{ id: user.id, nombre, apellido, correo: email }])
+      .insert([{ id: user.id, nombre, apellido, correo: email, acepta_terminos: terminos }])
       .select();
 
     if (dbError) throw dbError;
--- src/views/registro.vue
+++ src/views/registro.vue
@@ -1,5 +1,50 @@
 <script setup>
 import AuthLayout from '@/layouts/AuthLayout.vue'
+import { registerService } from '@/services/auth/register.js'
+import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+
+const form = reactive({
+  nombre: '',
+  apellido: '',
+  email: '',
+  password: '',
+  terminos: false
+})
+
+const mensajeError = ref('')
+const loading = ref(false)
+
+const handleSubmit = async () => {
+  loading.value = true
+  mensajeError.value = '';
+
+  if (!form.terminos) {
+    mensajeError.value = 'Debes aceptar los términos.';
+    return;
+  }
+
+  try {
+    // Llamamos al servicio con los datos de nuestro objeto reactivo
+    await registerService(form.nombre, form.apellido, form.email, form.password, form.terminos);
+    // alert('¡Registro exitoso!');
+    Object.assign(form, {
+      nombre: '',
+      apellido: '',
+      email: '',
+      password: '',
+      terminos: false
+    })
+    router.push('/');
+  } catch (error) {
+    mensajeError.value = error.message;
+  } finally {
+    loading.value = false;
+  }
+};
+
 </script>
 
 <template>
@@ -14,44 +59,47 @@ import AuthLayout from '@/layouts/AuthLayout.vue'
     </div>
 
     <!-- Formulario -->
-    <form class="space-y-5">
+    <form @submit.prevent="handleSubmit" class="space-y-5">
 
       <div class="flex gap-4 flex-col md:flex-row">
         <!-- Nombre -->
         <div>
             <label class="block text-xs font-bold text-gray-700 mb-2">Nombre</label>
-            <input type="text" placeholder="Ingrese su nombre" class="w-full border border-gray-200 rounded-xl px-4 py-3 text-sm">
+            <input v-model="form.nombre" type="text" placeholder="Ingrese su nombre" class="w-full border border-gray-200 rounded-xl px-4 py-3 text-sm">
         </div>
 
         <!-- Apellido -->
         <div>
             <label class="block text-xs font-bold text-gray-700 mb-2">Apellido</label>
-            <input type="text" placeholder="Ingrese su apellido" class="w-full border border-gray-200 rounded-xl px-4 py-3 text-sm">
+            <input v-model="form.apellido" type="text" placeholder="Ingrese su apellido" class="w-full border border-gray-200 rounded-xl px-4 py-3 text-sm">
         </div>
       </div>
 
       <!-- Correo -->
       <div>
         <label class="block text-xs font-bold text-gray-700 mb-2">Correo</label>
-        <input type="email" placeholder="Ingrese su correo"
+        <input v-model="form.email" type="email" placeholder="Ingrese su correo"
           class="w-full border border-gray-200 rounded-xl px-4 py-3 text-sm">
       </div>
 
       <!-- Password -->
       <div>
         <label class="block text-xs font-bold text-gray-700 mb-2">Contraseña</label>
-        <input type="password" placeholder="Ingrese su contraseña"
+        <input v-model="form.password" type="password" placeholder="Ingrese su contraseña"
           class="w-full border border-gray-200 rounded-xl px-4 py-3 text-sm">
       </div>
 
       <!-- Términos -->
       <div class="flex items-center gap-2 text-xs text-gray-500">
-        <input type="checkbox">
+        <input v-model="form.terminos" type="checkbox">
         <span>Acepto términos y condiciones</span>
       </div>
 
+      <!-- Mostrar error si existe -->
+      <p v-if="mensajeError" class="text-red-500 text-xs">{{ mensajeError }}</p>
+
       <!-- Botón -->
-      <button class="w-full bg-indigo-500 text-white py-3 rounded-xl">
+      <button :disabled="loading" type="submit" class="w-full bg-indigo-500 text-white py-3 rounded-xl">
         Registrarse
       </button>
 
